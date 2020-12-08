b108eb8f9678026a247ac2eb7dd105f421e33a3c
halowe <haleylowe0@gmail.com>
2020-12-07 23:57:22 -0800

    returned the ListItem div, including heading, paragraph, and Button tags

--- client/src/components/Book/Book.js
+++ client/src/components/Book/Book.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ListItem from "../BookList/BookList";
+import Button from "react-bootstrap/Button";
 
 //start Book variable
 let Book = ({ title, subtitle, authors, link, description, image, Button }) => {
@@ -20,5 +22,40 @@ let Book = ({ title, subtitle, authors, link, description, image, Button }) => {
   };
 
   const trimDescription = trimText(description, 255, "...");
+
+  return (
+    <ListItem>
+      <div className="media-snippet">
+        <article className="media">
+          <figure className="media-left">
+            <p>
+              <img className="image is-128auto" src={image} alt={title} />
+            </p>
+          </figure>
+          <div className="media-content">
+            <h3 className="heading-title">{title}</h3>
+            {subtitle && <h5 className="heading-subtitle">{subtitle}</h5>}
+            <p className="heading-subtitle">by {authors} (Author)</p>
+            <p className="p-article">{trimDescription}</p>
+            <div calssName="d-flex flex-row bd-highlight mb-3">
+              <div className="p-2 bd-highlight">
+                <a
+                  href={link}
+                  className="btn view-button heading-subtitle ml-2"
+                  target="__blank"
+                  rel="noopener noreferrer"
+                >
+                  View
+                </a>
+              </div>
+              <div className="p-2 bd-highlight">
+                <Button />
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    </ListItem>
+  );
 };
 //end function
--- client/src/components/BookList/BookList.js
+++ client/src/components/BookList/BookList.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const List = ({}) => <ul className="list-group">({children})</ul>;
+export const BookList = ({}) => <ul className="list-group">({children})</ul>;
 
 export function ListItem({ children }) {
   return <li className="list-group-item">{children}</li>;
