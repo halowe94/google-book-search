7e3c1481a14102732822fa520ca933a88aaa71d6
halowe <haleylowe0@gmail.com>
2020-12-13 22:03:30 -0800

    findALl and findById function in BookCon.js

--- controllers/BookCon.js
+++ controllers/BookCon.js
@@ -1,9 +1,17 @@
-// const db = require("../models");
+const db = require("../models");
 
 module.exports = {
-  findAll: function (req, res) {},
+  findAll: function (req, res) {
+    db.Book.find(req.query)
+      .then((dbBook) => res.json(dbBook))
+      .catch((err) => res.status(422).json(err));
+  },
 
-  findById: function (req, res) {},
+  findById: function (req, res) {
+    db.Book.findById(req.params.id)
+      .then((dbBook) => res.json(dbBook))
+      .catch((err) => res.status(422).json(err));
+  },
 
   create: function (req, res) {},
 
