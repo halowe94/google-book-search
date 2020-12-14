6090ec7a29a65365b9318b95302f419bf7be5b62
halowe <haleylowe0@gmail.com>
2020-12-13 20:46:44 -0800

    added onCHange and onClick to SearchForm

--- client/src/components/SearchForm/SearchForm.js
+++ client/src/components/SearchForm/SearchForm.js
@@ -1,13 +1,28 @@
 import React from "react";
 
-function SearchForm({ q, handleInputChange, handleFromSubmit }) {
+function SearchForm({ q, handleInputChange, handleFormSubmit }) {
   return (
-    <form>
-      <div>
-        <label></label>
-        <input />
+    <form className="form-inline" role="form">
+      <div className="form-group mx-sm-3 mb-2">
+        <label htmlFor="Title" className="sr-only"></label>
+        <input
+          className="form-control heading-subtitle"
+          id="Title"
+          type="text"
+          placeholder="Book"
+          name="q"
+          onChange={handleInputChange}
+          size="55"
+          required
+        />
       </div>
-      <button></button>
+      <button
+        onClick={handleFormSubmit}
+        type="submit"
+        className="btn btn-lg search-button heading-subtitle"
+      >
+        SEARCH
+      </button>
     </form>
   );
 }
