b558a475517aadca3fc630bdb8151ed80fa68a9c
halowe <haleylowe0@gmail.com>
2020-12-26 23:09:29 -0800

    index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,14 @@
+const path = require("path");
+const router = require("express").Router();
+const booksRoutes = require("./book");
+const googleRoutes = require("./google");
 
+router.use("/books", booksRoutes);
+
+router.use("/google", googleRoutes);
+
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+modeule.exports = router;
