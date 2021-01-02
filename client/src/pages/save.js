1d8ed61af1097a513c6da81b8af40e3f9097e8e7
halowe <haleylowe0@gmail.com>
2021-01-02 00:01:42 -0800

    imported components in search.js

--- client/src/pages/save.js
+++ client/src/pages/save.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { toast } from "react-toastify";
 import { List } from "../components/BookList/BookList";
 import Book from "../components/Book/Book";
-// import API from "../utils/API";
+import API from "../utils/API";
 
 class Save extends Component {
   state = {
---
+++ client/src/pages/search.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import {toast } from react-toastify;
+import Book from "../components/Book/index";
+import SearchForm from "../components/SearchForm/SearchForm";
+import { BookList } from "../components/BookList/BookList";
+import API from "../utils/API";
